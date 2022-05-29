60f234017f501988ed23ccdecd6e919d201ff1f3
Mako <makofuel3d@gmail.com>
2022-05-29 18:41:03 +0100

    bishop legal move check [no other pieces]

--- Scene/Scenes/Bishop/Bishop.gd
+++ Scene/Scenes/Bishop/Bishop.gd
@@ -17,6 +17,8 @@ onready var PIECE = $Piece
 
 onready var BASE_CHILD = $Base_Piece
 
+var flag = false
+
 func _ready():
 	pass
 	
@@ -25,7 +27,7 @@ func _physics_process(delta):
 	
 	
 	if Input.is_action_just_pressed("ui_accept"):
-		BASE_CHILD.Move_to_Tile(5,7)
+		BASE_CHILD.Move_to_Tile(3,4)
 	
 	if Input.is_action_just_pressed("left_mouse"):
 		selected = BASE_CHILD.CheckSelected()
@@ -42,7 +44,46 @@ func _physics_process(delta):
 #usedatamap 
 	
 func CheckLegalMoves():
-	print("lel")
+#	print("lel")
+	#Global.MAP_GRID.data_map[y][x][0]
 	
+	#check up and left
+	var checkedPosition = BASE_CHILD.Location()
+
+	if (flag == false):
+		print("valid positions left and up")
+		while checkedPosition.x > 0 and checkedPosition.y > 0: #checks up and left
+		#	count += 1
+			checkedPosition += Vector2(-1, -1)
+			print(String(checkedPosition) + " is valid")
+			
+			
+	checkedPosition = BASE_CHILD.Location()
+	if (flag == false):
+		print("valid positions right and down")
+		while checkedPosition.x < 7 and checkedPosition.y < 7: 
+		#	count += 1
+			checkedPosition += Vector2(1, 1)
+			print(String(checkedPosition) + " is valid")
+			
 	
+	checkedPosition = BASE_CHILD.Location()
+	if (flag == false):
+		print("valid positions right and up")
+		while checkedPosition.x < 7 and checkedPosition.y > 0:
+		#	count += 1
+			checkedPosition += Vector2(1, -1)
+			print(String(checkedPosition) + " is valid")
+			
+	checkedPosition = BASE_CHILD.Location()
+	if (flag == false):
+		print("valid positions left and down")
+		while checkedPosition.x > 0 and checkedPosition.y < 7:
+		#	count += 1
+			checkedPosition += Vector2(-1, 1)
+			print(String(checkedPosition) + " is valid")	
+			
+			
+	flag = true
+		
 
